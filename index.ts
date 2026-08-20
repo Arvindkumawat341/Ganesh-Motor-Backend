8db999178152c3a32f80497dea4006f51fa5893e
Arvindkumawat341 <arvind.kumawat@maitriiinfotech.com>
2026-08-20 12:45:26 +0530

    fix: update code

--- index.ts
+++ index.ts
@@ -8,11 +8,7 @@ connectDB();
 
 const port = process.env.PORT || 6000;
 app.listen(port, () => {
-<<<<<<< HEAD
   console.log(`Server running on port ${port}`);
-=======
-  console.log(`Server running on http://localhost:${port}`);
->>>>>>> 2d09e33e1f07181e7561c9c6e9a32b09bd8db8d5
 });
 
 export default app;
